37b3e35899530acd07e25e08237c45d6ee79b0a9
hed-dsanchez-dev <hed-dsanchez@smu.edu.ph>
2025-11-07 08:43:39 +0800

    change the code in write button

--- Laboratory Exercise 10/Form1.Designer.vb
+++ Laboratory Exercise 10/Form1.Designer.vb
@@ -22,10 +22,89 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Sort = New System.Windows.Forms.Button()
+        Me.Write = New System.Windows.Forms.Button()
+        Me.Read = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(102, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Sort
+        '
+        Me.Sort.Location = New System.Drawing.Point(6, 185)
+        Me.Sort.Name = "Sort"
+        Me.Sort.Size = New System.Drawing.Size(75, 23)
+        Me.Sort.TabIndex = 1
+        Me.Sort.Text = "Sort"
+        Me.Sort.UseVisualStyleBackColor = True
+        '
+        'Write
+        '
+        Me.Write.Location = New System.Drawing.Point(6, 55)
+        Me.Write.Name = "Write"
+        Me.Write.Size = New System.Drawing.Size(75, 23)
+        Me.Write.TabIndex = 2
+        Me.Write.Text = "Write"
+        Me.Write.UseVisualStyleBackColor = True
+        '
+        'Read
+        '
+        Me.Read.Location = New System.Drawing.Point(87, 185)
+        Me.Read.Name = "Read"
+        Me.Read.Size = New System.Drawing.Size(75, 23)
+        Me.Read.TabIndex = 3
+        Me.Read.Text = "Read"
+        Me.Read.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(6, 84)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Read)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.Sort)
+        Me.GroupBox1.Controls.Add(Me.Write)
+        Me.GroupBox1.Location = New System.Drawing.Point(310, 55)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 233)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sorting number"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Sort As Button
+    Friend WithEvents Write As Button
+    Friend WithEvents Read As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
--- Laboratory Exercise 10/Form1.vb
+++ Laboratory Exercise 10/Form1.vb
@@ -1,3 +1,63 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+
+    Dim filePath As String = "sample.txt"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Read_Click(sender As Object, e As EventArgs) Handles Read.Click
+        ListBox1.Items.Clear()
+        Using reader As New StreamReader(filePath)
+            Dim line As String
+            line = reader.ReadLine()
+            While (line IsNot Nothing)
+                ListBox1.Items.Add(line)
+                line = reader.ReadLine()
+            End While
+        End Using
+
+    End Sub
+
+    Private Sub Sort_Click(sender As Object, e As EventArgs) Handles Sort.Click
+        ListBox1.Items.Clear()
+
+        If Not File.Exists(filePath) Then
+            MessageBox.Show("No numbers found. Please add some first.")
+            Return
+        End If
+        Dim numbers As New List(Of Integer)
+        Using reader As New StreamReader(filePath)
+            While Not reader.EndOfStream
+                Dim line As String = reader.ReadLine()
+                Dim num As Integer
+                If Integer.TryParse(line, num) Then
+                    numbers.Add(num)
+                End If
+            End While
+        End Using
+        Dim sortedNumbers = numbers.OrderBy(Function(n) n)
+        For Each n In sortedNumbers
+            ListBox1.Items.Add(n)
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Write.Click
+        Dim input As Integer
+        If Integer.TryParse(TextBox1.Text, input) Then
+
+            Using writer As New StreamWriter(filePath, True)
+                writer.WriteLine(input)
+            End Using
+
+            MessageBox.Show("Number saved to file.")
+            TextBox1.Clear()
+
+        Else
+            MessageBox.Show("Please enter a valid number.")
+        End If
+    End Sub
 End Class
+
